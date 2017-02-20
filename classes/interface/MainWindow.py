1e3eb40225ba6b6e118fb776566752dce2fe06a5
Anthony Chappuis <anthony.chappuis@gmail.com>
2017-02-20 17:20:14 +0100

    Moved methods and widgets relative to the themes from main window to their own class Themes.py

--- classes/interface/MainWindow.py
+++ classes/interface/MainWindow.py
@@ -8,17 +8,20 @@
 #---------------------------------
 
 from constants import *
-from classes.interface.Text import Text
 
 import os
-from classes.library.Library import Library
+
+from classes.interface.Text import Text
 from classes.interface.Playlist import Playlist
+from classes.interface.Themes import Themes
+
+from classes.library.Library import Library
 
 from PyQt5 import Qt, QtGui, QtCore
 from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtWidgets import (QMainWindow, QApplication, QAction, qApp,
     QHBoxLayout, QSplitter, QWidget, QListWidget, QLabel,
-    QPushButton, QVBoxLayout, QGridLayout, QInputDialog)
+    QPushButton, QVBoxLayout, QGridLayout)
 
 class MainWindow(QMainWindow):
 
@@ -31,19 +34,15 @@ class MainWindow(QMainWindow):
         self.setWindowIcon(QIcon('dragonShout.png'))
 
         self.text = ''
+        self.changeLanguage()
 
         self.library = ''
         self.loadLibrary()
 
-        self.themesLayout = QVBoxLayout()
-        self.themesLayout.setAlignment(Qt.Qt.AlignHCenter)
-        self.themesWidget = QWidget()
-        self.themesWidget.setLayout(self.themesLayout)
-
-        self.playlist = ''
+        self.themes = Themes(self)
+        self.playlist = Playlist(self.text)
 
         self.menuBar()
-        self.changeLanguage()
 
         self.setGUI()
 
@@ -115,11 +114,9 @@ class MainWindow(QMainWindow):
         # mainHorizontalSplitter.addWidget(sceneVerticalSplitter)
 
         #Theme selection and controls
-        self.setThemes()
-        mainHorizontalSplitter.addWidget(self.themesWidget)
+        mainHorizontalSplitter.addWidget(self.themes)
 
         #Playlist
-        self.playlist = Playlist(self.text)
         mainHorizontalSplitter.addWidget(self.playlist)
 
         #adding the splitter containing the main elements to the window
@@ -140,51 +137,6 @@ class MainWindow(QMainWindow):
         	self.library = Library.load(filepath)
         else:
             self.library = Library("new_library","")
-
-    def setThemes(self):
-        """Gets the category objects in the library and arrange them as push buttons in the main window.
-           Takes no parameters.
-        """
-        newThemeButton = QPushButton('+')
-        newThemeButton.clicked.connect(lambda *args: self.addTheme())
-        newThemeButton.setMaximumWidth(100)
-        self.themesLayout.addWidget(newThemeButton)
-
-        for theme in self.library.categories:
-            self.addThemeButtonToThemeLayout(theme.name)
-
-
-    def selectTheme(self,themeName:str):
-        """Update the playlist with the music list of the selected theme
-            Takes one parameter:
-            - themeName as string
-        """
-        theme = self.library.get_category(themeName)
-        if theme :
-            self.playlist.setList(themeName,theme.tracks)
-
-    def addTheme(self):
-        """Add a new theme to the application and open dialog box to set the theme name.
-            Takes no parameter.
-        """
-        themeName, ok = QInputDialog.getText(self,self.text.localisation('dialogBoxes','newTheme','caption'),self.text.localisation('dialogBoxes','newTheme','question'))
-
-        if ok :
-            self.library.add_category(themeName)
-            self.addThemeButtonToThemeLayout(themeName)
-
-    def addThemeButtonToThemeLayout(self,buttonText:str=''):
-        """Adds a new theme button to the theme layout.
-            Takes one parameter:
-            - button text as string.
-        """
-        if buttonText == '' :
-            buttonText = self.text.localisation('buttons','newTheme','caption')
-
-        themeButton = QPushButton(buttonText)
-        themeButton.setMaximumWidth(100)
-        themeButton.clicked.connect(lambda *args: self.selectTheme(self.sender().text()))
-        self.themesLayout.addWidget(themeButton)
 
     def renameTheme(self,themeName:str):
         """Modify the name of the theme.
---
+++ classes/interface/Themes.py
@@ -0,0 +1,54 @@
+#---------------------------------
+#Author: Chappuis Anthony
+#
+#Class responsible for the themes' collection of widget used in the main window
+#
+#Application: DragonShout music sampler
+#Last Edited: February 20th 2017
+#---------------------------------
+
+from classes.interface import MainWindow
+
+from PyQt5 import Qt
+from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout, QInputDialog
+
+class Themes(QWidget):
+
+    def __init__(self, mainWindow:MainWindow):
+        super().__init__()
+
+        self.mainWindow = mainWindow
+        self.layout = QVBoxLayout()
+        self.layout.setAlignment(Qt.Qt.AlignHCenter)
+        self.setLayout(self.layout)
+
+        newThemeButton = QPushButton('+')
+        newThemeButton.clicked.connect(lambda *args: self.addTheme())
+        newThemeButton.setMaximumWidth(100)
+        self.layout.addWidget(newThemeButton)
+
+        for theme in self.mainWindow.library.categories:
+            self.addTheme(theme.name)
+
+    def addTheme(self):
+        """Adds a new theme button to the theme layout.
+            Takes no parameter.
+        """
+        themeName, ok = QInputDialog.getText(self,self.mainWindow.text.localisation('dialogBoxes','newTheme','caption'),self.mainWindow.text.localisation('dialogBoxes','newTheme','question'))
+
+        if ok :
+            self.mainWindow.library.add_category(themeName)
+
+            themeButton = QPushButton(themeName)
+            themeButton.setMaximumWidth(100)
+            themeButton.clicked.connect(lambda *args: self.selectTheme(self.sender().text()))
+            self.layout.addWidget(themeButton)
+
+    def selectTheme(self,themeName:str):
+        """Update the playlist with the music list of the selected theme
+            Takes one parameter:
+            - themeName as string
+        """
+        theme = self.mainWindow.library.get_category(themeName)
+        if theme :
+            self.mainWindow.playlist.setList(themeName,theme.tracks)
